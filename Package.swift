81f3040a6ee7625a913ba6aa0f614d8a6819afc7
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-13 03:18:36 +0530

    Updated for release 3.0.0

--- Package.swift
+++ Package.swift
@@ -15,12 +15,13 @@ let package = Package(
         .target(
             name: "Punycode_Aggregation",
             dependencies: ["Punycode"],
+            packageAccess: false,
             swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
         ),
         .binaryTarget(
             name: "Punycode",
-            url: "https://github.com/swift-precompiled/PunycodeSwift/releases/download/3.0.0/Punycode-c5b9bfdcb3606b43c351d1d40544c6642f437a9307d0acfab47b9a8bf6af3c38.xcframework.zip",
-            checksum: "c5b9bfdcb3606b43c351d1d40544c6642f437a9307d0acfab47b9a8bf6af3c38"
+            url: "https://github.com/swift-precompiled/PunycodeSwift/releases/download/3.0.0/Punycode-84b3358da077aaac42168ef5498eea4d67c4f938407a76f31b4b9b8b9788d495.xcframework.zip",
+            checksum: "84b3358da077aaac42168ef5498eea4d67c4f938407a76f31b4b9b8b9788d495"
         )
     ]
 )
